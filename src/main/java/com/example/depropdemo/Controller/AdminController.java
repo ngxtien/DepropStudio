b7b05480b0cf30f03e22678999ea4dbaeaff2511
Jayceee692 <108479773+Jayceee692@users.noreply.github.com>
2024-05-23 13:34:34 +0700

    ...

---
+++ src/main/java/com/example/depropdemo/Controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.example.depropdemo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/admin/dashboard/")
+    public String adminPage() {
+        return "admin";
+    }
+}
