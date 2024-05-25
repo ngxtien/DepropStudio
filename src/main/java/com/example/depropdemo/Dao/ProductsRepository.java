1c80ae0c12679ba532b901d78911648f6c382c59
DipTN <dieptuanngoc@gmail.com>
2024-05-25 14:12:57 +0700

    admin

--- src/main/java/com/example/depropdemo/Dao/ProductsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.depropdemo.Dao;
-
-import com.example.depropdemo.Model.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductsRepository extends JpaRepository<Products, Long> {
-}
--- src/main/java/com/example/depropdemo/Dao/TrackingRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.depropdemo.Dao;
-
-public interface TrackingRepository {
-}
