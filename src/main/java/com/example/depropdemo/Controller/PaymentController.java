9782039ab3568d14511ff82c3807ff89178ddffa
Ngoc Tien <npntxa@gmail.com>
2024-06-25 16:40:29 +0700

    Loading...

--- src/main/java/com/example/depropdemo/Controller/PaymentController.java
+++ src/main/java/com/example/depropdemo/Controller/PaymentController.java
@@ -14,38 +14,38 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
-  private final PayOS payOS;
-
-  public PaymentController(PayOS payOS) {
-    super();
-    this.payOS = payOS;
-
-  }
-
-  @PostMapping(path = "/payos_transfer_handler")
-  public ObjectNode payosTransferHandler(@RequestBody ObjectNode body) {
-
-    ObjectMapper objectMapper = new ObjectMapper();
-    ObjectNode respon = objectMapper.createObjectNode();
-
-    try {
-      //Init Response
-      respon.put("error", 0);
-      respon.put("message", "Ok");
-      respon.set("data", null);
-
-      JsonNode data = payOS.verifyPaymentWebhookData(body);
-      System.out.println(data);
-      if (Objects.equals(data.get("description").asText(), "Ma giao dich thu nghiem")){
-        return respon;
-      }
-      return respon;
-    } catch (Exception e) {
-      e.printStackTrace();
-      respon.put("error", -1);
-      respon.put("message", e.getMessage());
-      respon.set("data", null);
-      return respon;
-    }
-  }
+//  private final PayOS payOS;
+//
+//  public PaymentController(PayOS payOS) {
+//    super();
+//    this.payOS = payOS;
+//
+//  }
+//
+//  @PostMapping(path = "/payos_transfer_handler")
+//  public ObjectNode payosTransferHandler(@RequestBody ObjectNode body) {
+//
+//    ObjectMapper objectMapper = new ObjectMapper();
+//    ObjectNode respon = objectMapper.createObjectNode();
+//
+//    try {
+//      //Init Response
+//      respon.put("error", 0);
+//      respon.put("message", "Ok");
+//      respon.set("data", null);
+//
+//      JsonNode data = payOS.verifyPaymentWebhookData(body);
+//      System.out.println(data);
+//      if (Objects.equals(data.get("description").asText(), "Ma giao dich thu nghiem")){
+//        return respon;
+//      }
+//      return respon;
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//      respon.put("error", -1);
+//      respon.put("message", e.getMessage());
+//      respon.set("data", null);
+//      return respon;
+//    }
+//  }
 }
