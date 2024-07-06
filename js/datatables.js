cf1ce19fd798fb038139b6874d5cf87fab5d3df9
DipTN <dieptuanngoc@gmail.com>
2024-07-07 06:09:15 +0700

    update footer

--- js/datatables.js
+++ js/datatables.js
@@ -1,14 +1,26 @@
-new DataTable('#productTable', {
-    columnDefs: [
-        {
-            orderable: false,
-            render: DataTable.render.select(),
-            targets: 0
-        }
-    ],
-    select: {
-        style: 'multi+shift',
-        selector: 'td'
-    },
-    order: [[1, 'asc']]
+$(document).ready(function() {
+    $('#productTable').DataTable({
+        // Customization options
+        paging: true,
+        searching: true,
+        ordering: true,
+        columnDefs: [
+            { 
+                orderable: false,
+                render: DataTable.render.select(),
+                targets: 0,
+            } 
+        ],
+        select: {
+            style: 'multi+shift',
+            selector: 'td'
+        },
+        order: [[1, 'asc']]
+    });
+
+    $('#deleteButton').on('click', function() {
+        var table = $('#productTable').DataTable();
+        table.rows('.selected').remove().draw();
+    });
 });
+
